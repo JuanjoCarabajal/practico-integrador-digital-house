dcdb2230ff6b64e12faec5be076e79c4fd04a5cc
JuanjoCarabajal <153025931+JuanjoCarabajal@users.noreply.github.com>
2023-12-05 19:42:46 -0300

    Cambiado al puerto 80

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
 
-app.listen(3000, function () {
-	console.log('Server is running on port 3000');
+app.listen(80, function () {
+	console.log('Server is running on port 80');
 });
